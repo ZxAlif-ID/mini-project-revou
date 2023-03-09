b06de553cc61de0258b4d0478555a9f51a6c90e9
Fillavar <alifnurhidayat84@gmail.com>
2023-03-09 21:25:02 +0700

    second commit

---
+++ js/validate.js
@@ -0,0 +1,16 @@
+const fullName = document.querySelector('.name')
+const email = document.querySelector('.email')
+const interest = document.querySelector('.interest')
+const submit = document.querySelector('.submit-form')
+
+submit.addEventListener('click', ()=> {
+    if (fullName.value == '' || email.value == '' || interest.value == '') {
+        alert('Fill your data correctly')
+    } else if (fullName.value.length < 5) {
+        alert('Enter a name of atleast 5 letters')
+    } else if(email.value.indexOf('@gmail.com') == -1){
+        alert('Enter the correct Email format')
+    } else {
+        alert('Your data has succesfully saved and sent')
+    }
+})
